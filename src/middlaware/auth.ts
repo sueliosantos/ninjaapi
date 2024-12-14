5e972d7e4bcda2e8cb119a68e811eaef5842ab77
Suélio Santos <suelio.santos@esig.com.br>
2024-12-14 20:40:16 -0300

    Add rotas publicas

--- src/middlaware/auth.ts
+++ src/middlaware/auth.ts
@@ -6,7 +6,17 @@ export default async (req: Request, res: Response, next: Function) => {
   let token =
     req.body.token || req.query.token || req.headers["x-token-access"];
 
-  if (token) {
+  let publicRoutes = <Array<String>>config.publicRoutes;
+  let isPublicRoute: boolean = false;
+
+  publicRoutes.forEach((url) => {
+    let isPublic = req.url.includes(url);
+    if (isPublic) {
+      isPublicRoute = true;
+    }
+  });
+  if (isPublicRoute) next();
+  else if (token) {
     try {
       let _userAuth = verify(token, config.secretyKey);
       req.userAuth = _userAuth;
